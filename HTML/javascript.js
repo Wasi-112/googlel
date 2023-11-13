07bc9c1ee8b8fb3c6852e240a1e86600323ce004
wasi <wasiraza112@gmail.com>
2023-11-13 09:09:06 -0800

    if statement practice

--- HTML/javascript.js
+++ HTML/javascript.js
@@ -1,11 +1,11 @@
 {
-const v1 ="string1";
-const v3 ="string3";
-{
-  console.log(v1);
-  const v2 ="string2";
-  const v3 = "string3";
+  const v1 ="string1";
+  const v3 ="string3";
+  {
+    console.log(v1);
+    const v2 ="string2";
+    const v3 = "string3";
+    console.log(v3);
+  }
   console.log(v3);
-}
-console.log(v3);
-}
+  }
